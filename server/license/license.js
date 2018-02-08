914de91e57b9ebd58c8eac96a6a733b3a8d5b63e
Daniel Levine <daniel_levine2@comcast.com>
2018-02-08 14:27:06 -0500

    Added busy work.

--- server/license/license.js
+++ server/license/license.js
@@ -10,9 +10,21 @@ function sleep_promise(ms)
     });
 }
 
+function busy_work(n)
+{
+    for (var i = 0; i < n; ++i)
+    {
+        var array = [];
+        for (var j = 0; j < 10000; ++j)
+            array.push(Math.random());
+        array.sort();
+    }
+    return BPromise.resolve(true);
+}
+
 exports.request_license = function(redis_read_db, redis_write_db, MVPD_id, user_id, rule_id, device_type, device_id, ip)
 {
-    return sleep_promise(500)
+    return busy_work(2)
     .return(true);
 };
 
