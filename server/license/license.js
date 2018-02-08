9a4d07b76a0264d456be7ddfb52d0f27db2efe63
Daniel Levine <daniel_levine2@comcast.com>
2018-02-08 14:21:53 -0500

    Added sleep prior to returning result.

--- server/license/license.js
+++ server/license/license.js
@@ -1,9 +1,19 @@
 // dependencies
 var BPromise = require("bluebird");
 
+function sleep_promise(ms)
+{
+    return new BPromise(function(resolve, reject){
+        setTimeout(function(){
+            resolve(true);
+        }, ms);
+    });
+}
+
 exports.request_license = function(redis_read_db, redis_write_db, MVPD_id, user_id, rule_id, device_type, device_id, ip)
 {
-    return BPromise.resolve(true);
+    return sleep_promise(500)
+    .return(true);
 };
 
 /*** END routes ***/
