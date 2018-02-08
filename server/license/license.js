233ee9b862a0dc0f7a2818ded63b99c1694041d1
Daniel Levine <daniel_levine2@comcast.com>
2018-02-08 14:29:01 -0500

    Made busy work easier.

--- server/license/license.js
+++ server/license/license.js
@@ -1,21 +1,21 @@
 // dependencies
 var BPromise = require("bluebird");
 
-function sleep_promise(ms)
+/*function sleep_promise(ms)
 {
     return new BPromise(function(resolve, reject){
         setTimeout(function(){
             resolve(true);
         }, ms);
     });
-}
+}*/
 
 function busy_work(n)
 {
     for (var i = 0; i < n; ++i)
     {
         var array = [];
-        for (var j = 0; j < 10000; ++j)
+        for (var j = 0; j < 1000; ++j)
             array.push(Math.random());
         array.sort();
     }
@@ -24,7 +24,7 @@ function busy_work(n)
 
 exports.request_license = function(redis_read_db, redis_write_db, MVPD_id, user_id, rule_id, device_type, device_id, ip)
 {
-    return busy_work(2)
+    return busy_work(1)
     .return(true);
 };
 
