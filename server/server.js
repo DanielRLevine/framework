f88bcd507ab529f707dea682c1e308984dac0d48
Daniel Levine <daniel_levine2@comcast.com>
2018-02-10 13:23:44 -0500

    Removed all promises.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 /*** BEGIN dependencies ***/
 
-var BPromise = require("bluebird");
 var http = require("http");
 var express = require("express");
 
@@ -30,25 +29,13 @@ app.use(function (req, res, next) {
     next();
 });
 
-function busy_work(n)
-{
-    for (var i = 0; i < n; ++i)
-    {
-        var array = [];
-        for (var j = 0; j < 1000; ++j)
-            array.push(Math.random());
-        array.sort();
-    }
-    return BPromise.resolve(true);
-}
-
 // callers
 function request_license(req, res) {
-    var result = busy_work(1)
-    .then(function(){
-        res.send(true);
-        return true;
-    });
+    var array = [];
+    for (var j = 0; j < 1000; ++j)
+        array.push(Math.random());
+    array.sort();
+    res.send(true);
 }
 
 // routes
