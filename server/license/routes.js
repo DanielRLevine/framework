4cd49faba4f61adeb377c18bee2682e1ea4a3e56
Daniel Levine <daniel_levine2@comcast.com>
2018-02-10 11:36:40 -0500

    Removed license.js.

--- server/license/routes.js
+++ server/license/routes.js
@@ -1,14 +1,33 @@
 // dependencies
+var BPromise = require("bluebird");
+
 var router = require("express").Router();
 var server = require("../server.js");
-var license = require("./license.js");
 
 // routes
-router.post("/", request_license);
+router.post("/", request_license_route);
+
+function busy_work(n)
+{
+    for (var i = 0; i < n; ++i)
+    {
+        var array = [];
+        for (var j = 0; j < 1000; ++j)
+            array.push(Math.random());
+        array.sort();
+    }
+    return BPromise.resolve(true);
+}
+
+function request_license()
+{
+    return busy_work(1)
+    .return(true);
+}
 
 // callers
-function request_license(req, res) {
-    var result = license.request_license();
+function request_license_route(req, res) {
+    var result = request_license();
     var msg = "LICENSE POST";
     return server.result_handler(res, result, "license", "Request License", "user not specified");
 }
