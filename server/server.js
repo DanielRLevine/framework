e57e64d3d50ece22735562f990e780d58c3e3069
Daniel Levine <daniel_levine2@comcast.com>
2018-02-10 15:36:56 -0500

    Split into helper function to hopefully trigger bug.

--- server/server.js
+++ server/server.js
@@ -3,13 +3,17 @@ var express = require("express");
 
 var app = express();
 
-function my_route(req, res) {
+function my_helper()
+{
     var array = [];
     for (var j = 0; j < 1000; ++j)
         array.push(Math.random());
     array.sort();
+    return Promise.resolve(true);
+}
 
-    Promise.resolve(true)
+function my_route(req, res) {
+    my_helper()
     .then(function(){
         res.send(true);
     });
